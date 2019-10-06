a0beb326086f7b16028dd247f5fac7218bebd22d
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-10-07 01:15:03 +0500

    asd

--- src/routing/router.js
+++ src/routing/router.js
@@ -17,28 +17,28 @@ import Donate from "../container/Payment_process/Donate";
 import Pay_denied from "../container/Payment_process/Pay_denied";
 import Pay_sucess from "../container/Payment_process/Pay_sucess";
 // Page not found
-import PageNotFound from "../container/PageNotFound";
+// import PageNotFound from "../container/PageNotFound";
 function AppRouter() {
   return (
     <Router>
-      <Route path="/" exact  component={props => <HomeScreen {...props} />} />
-      <Route path="/child/" component={props => <Child {...props} />} />
-      <Route path="/teacher/" component={props => <Teacher {...props} />} />
+      <Route path="http://kef.education/" exact  component={props => <HomeScreen {...props} />} />
+      <Route path="http://kef.education/child/" component={props => <Child {...props} />} />
+      <Route path="http://kef.education/teacher/" component={props => <Teacher {...props} />} />
       {/* Where we work */}
-      <Route path="/pvpc" exact component={props => <Pvpc {...props} />} />
-      <Route path="/bvpc" exact component={props => <Bvpc {...props} />} />
-      <Route path="/svpc" exact component={props => <Scpc {...props} />} />
-      <Route path="/jacte" exact component={props => <Jacte {...props} />} />
+      <Route path="http://kef.education/pvpc" exact component={props => <Pvpc {...props} />} />
+      <Route path="http://kef.education/bvpc" exact component={props => <Bvpc {...props} />} />
+      <Route path="http://kef.education/svpc" exact component={props => <Scpc {...props} />} />
+      <Route path="http://kef.education/jacte" exact component={props => <Jacte {...props} />} />
 
       {/* getInvolved */}
-      <Route path="/getInvolved" exact component={props => <GetInvolved {...props} />} />
-      <Route path="/News&Events" exact component={props => <NewsAndEvents {...props} />} />
-      <Route path="/contactus" exact component={props => <ContactUs {...props} />} />
+      <Route path="http://kef.education/getInvolved" exact component={props => <GetInvolved {...props} />} />
+      <Route path="http://kef.education/News&Events" exact component={props => <NewsAndEvents {...props} />} />
+      <Route path="http://kef.education/contactus" exact component={props => <ContactUs {...props} />} />
       {/* payments pages */}
-      <Route path="/donate" exact component={props => <Donate {...props} />} />
-      <Route path="/Pay" exact component={props => <Account_info {...props} />} />
-      <Route path="/paySucess" exact component={props => <Pay_sucess {...props} />} />
-      <Route path="/paymentNotSucess" exact component={props => <Pay_denied {...props} />} />
+      <Route path="http://kef.education/donate" exact component={props => <Donate {...props} />} />
+      <Route path="http://kef.education/Pay" exact component={props => <Account_info {...props} />} />
+      <Route path="http://kef.education/paySucess" exact component={props => <Pay_sucess {...props} />} />
+      <Route path="http://kef.education/paymentNotSucess" exact component={props => <Pay_denied {...props} />} />
     </Router>
   );
 }
